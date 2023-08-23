e9d6836729101df4ab5060732cc5a34d0d3f26a3
thidoa <thiagovini200@gmail.com>
2023-08-23 16:54:17 -0300

    style: algumas estilizações

--- controle_fluxo/forms.py
+++ controle_fluxo/forms.py
@@ -2,12 +2,12 @@ from django import forms
 from .models import Activities, Bank, Owner
 
 class ActivitiesForm(forms.Form):
-    value = forms.DecimalField(required=True)
-    input = forms.BooleanField(required=False)
-    description = forms.CharField(max_length=200, required=True, widget=forms.Textarea)
-    bank = forms.ModelChoiceField(queryset=Bank.objects.all())
-    owner = forms.ModelChoiceField(queryset=Owner.objects.all())
-    proof = forms.FileField(required=False)
+    value = forms.DecimalField(required=True, widget=forms.TextInput(attrs={'class':'bg-dark'}))
+    input = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'class':'bg-dark border border-light'}))
+    description = forms.CharField(max_length=200, required=True, widget=forms.Textarea(attrs={'class':'bg-dark'}))
+    bank = forms.ModelChoiceField(queryset=Bank.objects.all(), widget=forms.Select(attrs={'class':'bg-dark text-light'}))
+    owner = forms.ModelChoiceField(queryset=Owner.objects.all(), widget=forms.Select(attrs={'class':'bg-dark text-light'}))
+    proof = forms.FileField(required=False, widget=forms.ClearableFileInput(attrs={'class':'bg-dark text-light'}))
 
     def save(self):
         activities = Activities(
